77b3d44585077972cef8ce4548d8e4bac872c0fc
Alpha11201 <40555701+Alpha11201@users.noreply.github.com>
2025-07-14 14:59:47 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Fenigama Mollie Checkout Demo</title>
+</head>
+<body>
+    <h1>Bienvenue sur Fenigama Mollie Checkout Demo</h1>
+    <p>Utilisez le script create-payment.php pour tester le paiement.</p>
+</body>
+</html>
